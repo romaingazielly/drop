8cef509a696cb3a872442bfb86f4fb7909db9a2d
root <root@vps-33a91039.vps.ovh.net>
2021-06-22 16:04:45 +0000

    wp-config prod/local

--- wp-config.php
+++ wp-config.php
@@ -46,15 +46,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'put your unique phrase here' );
-define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-define( 'NONCE_KEY',        'put your unique phrase here' );
-define( 'AUTH_SALT',        'put your unique phrase here' );
-define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-define( 'NONCE_SALT',       'put your unique phrase here' );
-
+define('AUTH_KEY',         'V:bPdts|x@~|;NAgS*|f rq%z]4+4.oS+?>mj3)>%5BOXB;uZv#BtamWlg2@j2O@');
+define('SECURE_AUTH_KEY',  '~2UkxOcW&Rh^vI<{o@=}W}![d~^7n3&51,xs;DpsNJ>Z.izn.{${z5SFE8bBq85O');
+define('LOGGED_IN_KEY',    '(`lVEwbI6{H.b{!M5DPR3C%i*z%lz,Ty%=QP%?EyGHV5ka}+bU;A[H@.[*<*i<|F');
+define('NONCE_KEY',        'yd{Ke$kQ2Y)fvy2z2mUB}./km-x{qOk?[HXwz b5biz-]?FnUu=jA-IVJD^U`Z_b');
+define('AUTH_SALT',        '$vv~.hDZ@QEj(;En.P7:+f|QZ[(q$&? h<]@q<yoZ@V]P-}m%.x MK!: s >]F=i');
+define('SECURE_AUTH_SALT', '-~y$+KDosdDOCeA-P$7sU^~PwyIY./]MP!z|+^4p=ur^ F-+[zTa,9[pN^HOL%GI');
+define('LOGGED_IN_SALT',   'HR9|]^)hz~4rmA/EXADns?$_<.l82Prxecr2NOM@{vB6VGb:GTy2>In!kn4]apX0');
+define('NONCE_SALT',       '(Ds0Sk|TND9U9P&To+BH$-qq>+ao<:HhGW^y&|Vt;JBcAr@y{O1$i6m/C;4np.xq');
 /**#@-*/
 
 /**
@@ -81,7 +80,9 @@ define( 'WP_DEBUG', false );
 
 /* That's all, stop editing! Happy publishing. */
 
-@ini_set('upload_max_size' , '256M' );
+define( 'WP_HOME', 'https://drop.romain.ninja' );
+define( 'WP_SITEURL', 'https://drop.romain.ninja' );
+
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
